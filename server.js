5a9ee606c023206668e4254558bf4df7b3d7ed73
riya-k003 <riyakashyap0303@gmail.com>
2026-02-26 23:21:26 +0530

    Learned about inserting data through API

--- server.js
+++ server.js
@@ -2,8 +2,23 @@ const db = require('./server/db');
 const express = require('express');
 const app = express();
 
-app.get('/test', (req, res) => {
-    res.send("Server working");
+app.use(express.json());
+app.post('/register' , (req,res)=>{
+    const{name , email , password , role} = req.body;
+
+    const sql = "INSERT INTO users(name , email , password , role)VALUES(? , ? , ? , ?)";
+
+    db.query(sql , [name , email , password , role] , (err , result)=>{
+        if(err){
+            console.error(err);
+            return res.status(500).json({message: "Error inserting user"});
+        }
+        res.json({message: "User registered successfully"});
+    });
+});
+
+app.get('/register', (req, res) => {
+    res.send("Register route working (GET)");
 });
 
 app.listen(5000, () => {
